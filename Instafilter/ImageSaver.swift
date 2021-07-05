c53fd2970ec27bacb9f58b586b65f91eb18a78b5
VladimirMoor <81152453+VladimirMoor@users.noreply.github.com>
2021-07-05 14:57:46 +0300

    Initial Commit

---
+++ Instafilter/ImageSaver.swift
@@ -0,0 +1,30 @@
+//
+//  ImageSaver.swift
+//  Instafilter
+//
+//  Created by Vladimir on 29.06.2021.
+//
+
+import UIKit
+
+class ImageSaver: NSObject {
+    var successHandler: (() -> Void)?
+    var errorHandler: ((Error) -> Void)?
+    
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveError), nil)
+    }
+
+    @objc func saveError(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        
+        if let error = error {
+            errorHandler?(error)
+        } else {
+            successHandler?()
+        }
+
+    }
+    
+    
+    
+}
---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Vladimir on 28.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
